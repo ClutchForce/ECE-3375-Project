516c071b50a7ca84202b3026e01f332f23d907ed
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-04-01 17:19:39 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "address_map_arm.h"
 #include "Feature1.h"
@@ -9,6 +10,10 @@
 #include "Feature3.h"
 
 void logFeatureResponsibility(char *featureDesc);
+
+volatile unsigned int const hex0 = (unsigned int*) HEX3_Hex0_BASE;
+
+
 
 int main(void) {
     // Initialization
@@ -55,6 +60,47 @@ void updateGameState() {
     // Implement game state updates, possibly delegating to feature-specific functions
 }
 
-void provideFeedback() {
-    // Implement feedback provision, possibly delegating to feature-specific functions
+void generateSequence() {
+    //Command array
+    const cmds = ["Bop", "Twist", "Pull"];
+
+    //length of sequence
+    const length = rand()%3;
+
+    //Initialize sequence
+    const seq = [];
+
+        //Fill the sequence with commands
+        for(int i =0; i<length; i++){
+            const randNum = rand()%3;
+            seq[i]=cmds[randNum];
+        }
+
 }
+
+void Display(time, command){
+    //If time isn't 0 display command on hex
+    while (time!=0)
+    {
+        DisplayHex(command);
+    }
+    
+    //If time is 0 clear hex
+    if(time==0){
+        *hex0=0;
+    }
+}
+
+void DisplayHex(command){
+    switch(command){
+        case "Bop":
+            *hex0 = 0b01111100;
+        break;
+        case "Twist":
+            *hex0 =  0b01010000;
+        break;
+        case "Pull":
+            *hex0 =0b01110011;
+        break;
+    }
+}    
