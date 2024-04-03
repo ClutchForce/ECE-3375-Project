8209bb2ba7101382aa80e30ea3baf918b443e978
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2024-04-03 11:40:46 -0400

    Update main_v2.c

--- main_v2.c
+++ main_v2.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <stdlib.h> 
 #include <stdio.h>
-#include "audio_data.h" //audio data
 	
 #define HEX3_HEX0_BASE (uint32_t *) 0xFF200020
 #define HEX5_HEX4_BASE (uint32_t *) 0xFF200030
@@ -28,6 +27,22 @@
 bool Display(int time, const char* command);
 void DisplayHex(const char* command);
 
+//audio data
+#if DEMO
+const unsigned int pop_it_data[] = {
+    0x012D, 0x021E, 0x01C6, 0x01CE, 0x01A6, 0x01A9, 0x0182, 0x014E,
+};
+
+const unsigned int twist_it_data[] = {
+    /* ... audio sample data for "twist" sound ... */
+    0x0103, 0x00B6, 0x004F, 0xFFE5, 0xFFA1, 0xFF7A, 0xFF5D, 0xFF2F,
+};
+
+const unsigned int pull_it_data[] = {
+    /* ... audio sample data for "pull" sound ... */
+    0x00F7, 0x00A3, 0x0064, 0x002D, 0x0004, 0xFFE5, 0xFFD4, 0xFFD0,
+};
+#endif
 
 bool game_running = true;
 
